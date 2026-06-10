ded0dbce03793929bdda7bc7f72871e1bb326618
boy397 <aryamukherjee397@outlook.com>
2026-06-11 01:28:50 +0530

    pdf fix

--- src/server.ts
+++ src/server.ts
@@ -47,8 +47,8 @@ app.use((req: Request, res: Response, next) => {
     next();
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Session Configuration
 app.use(session({
